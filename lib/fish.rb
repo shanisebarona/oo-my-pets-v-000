a9e4d609fb7fec2e010daad764fd4f765b00c04e
Shanise Barona <shanisebarona@gmail.com>
2018-08-30 20:04:04 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 class Fish
-  # code goes here
+  
+  attr_accessor :mood
+  attr_reader :name
+ 
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,73 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @name = name
+    @species = species
+    @@all << self #keeps track of all instances that have been created upon init
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+  
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+  
+  def walk_dogs
+    # binding.pry
+    pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+  
+  def play_with_cats 
+    pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+  
+  def feed_fish
+    pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+    
+  def sell_pets
+    pets.each do |type, pets| 
+      pets.each do |pet| pet.mood = "nervous"
+      end
+    end
+    pets.clear
+  end
+  
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+    binding.pry
+  end
+ 
+  
+  
+  
 end
